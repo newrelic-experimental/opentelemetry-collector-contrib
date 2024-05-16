f1da3efdf1b02ce2a3c3a98f8f922396e9ec9bf2
Kartik Pattaswamy <kpattaswamy@pixielabs.ai>
2024-05-16 19:47:03 +0000

    initial setup to get service name attribute for pods
    Signed-off-by: Kartik Pattaswamy <kpattaswamy@pixielabs.ai>

--- processor/k8sattributesprocessor/internal/kube/kube.go
+++ processor/k8sattributesprocessor/internal/kube/kube.go
@@ -23,6 +23,7 @@ const (
 	tagStartTime            = "k8s.pod.start_time"
 	tagHostName             = "k8s.pod.hostname"
 	tagClusterUID           = "k8s.cluster.uid"
+	tagServiceName          = "k8s.service.name"
 	// MetadataFromPod is used to specify to extract metadata/labels/annotations from pod
 	MetadataFromPod = "pod"
 	// MetadataFromNamespace is used to specify to extract metadata/labels/annotations from namespace
@@ -113,6 +114,7 @@ type Pod struct {
 	Namespace   string
 	NodeName    string
 	HostNetwork bool
+	ServiceName string
 
 	// Containers specifies all containers in this pod.
 	Containers PodContainers
@@ -206,6 +208,7 @@ type ExtractionRules struct {
 	PodUID             bool
 	PodHostName        bool
 	PodIP              bool
+	PodServiceName     bool
 	ReplicaSetID       bool
 	ReplicaSetName     bool
 	StatefulSetUID     bool
